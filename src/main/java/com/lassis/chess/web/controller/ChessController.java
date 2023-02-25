cf768746e1b2c352b71a41a26a8518409b304970
Leandro Assis <leassis@yahoo.com.br>
2023-02-25 17:34:33 +0100

    simple multi game support

--- src/main/java/com/lassis/chess/web/controller/ChessController.java
+++ src/main/java/com/lassis/chess/web/controller/ChessController.java
@@ -19,70 +19,105 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.List;
+import java.util.Map;
 import java.util.Objects;
+import java.util.UUID;
+import java.util.concurrent.ConcurrentHashMap;
+
+import static com.lassis.chess.web.controller.ObjectTransformer.POINTINFO_COMPARATOR;
 
 @Controller
 @RequiredArgsConstructor
 @RequestMapping("/api")
 class ChessController {
+    private final Map<UUID, ChessGame> games = new ConcurrentHashMap<>();
     private final ObjectTransformer transformer;
 
-    private ChessGame chessGame = new ChessGame();
+    @DeleteMapping("/game/{id}")
+    ResponseEntity<Void> reset(@PathVariable("id") UUID id) {
+        var game = games.get(id);
+        if (Objects.isNull(game)) {
+            return ResponseEntity.notFound().build();
+        }
 
-    @DeleteMapping("/game")
-    ResponseEntity<Void> reset() {
-        chessGame = new ChessGame();
+        games.remove(id);
         return ResponseEntity.noContent().build();
     }
 
-    @GetMapping("/status")
-    ResponseEntity<StatusDTO> status() {
-        return ResponseEntity.ok(transformer.toStatusInfo(chessGame.getStatus()));
+    @PostMapping("/game")
+    ResponseEntity<Void> newGame() {
+        UUID id = UUID.randomUUID();
+        games.put(id, new ChessGame());
+        return ResponseEntity.noContent().build();
     }
 
-    @PostMapping("/moves")
-    ResponseEntity<PieceDTO> move(@RequestBody @Valid MoveDTO info) {
+    @GetMapping("/game/{id}/status")
+    ResponseEntity<StatusDTO> status(@PathVariable("id") UUID id) {
+        var game = games.get(id);
+        return Objects.nonNull(game)
+                ? ResponseEntity.ok(transformer.toStatusInfo(game.getStatus()))
+                : ResponseEntity.badRequest().build();
+    }
+
+    @PostMapping("/game/{id}/moves")
+    ResponseEntity<PieceDTO> move(@PathVariable("id") UUID id, @RequestBody @Valid MoveDTO info) {
+        var game = games.get(id);
+        if (Objects.isNull(game)) {
+            return ResponseEntity.notFound().build();
+        }
+
         Point from = transformer.toPoint(info.from());
         Point to = transformer.toPoint(info.to());
         if (Objects.isNull(from) || Objects.isNull(to)) {
             return ResponseEntity.badRequest().build();
         }
 
-        ChessGameStatus status = chessGame.moveTo(from, to);
+        ChessGameStatus status = game.moveTo(from, to);
 
-        return chessGame.at(to)
-                        .filter(v -> v.piece().isPresent())
-                        .map(transformer::toPieceInfo)
-                        .map(body -> ResponseEntity.ok()
-                                                   .header("black", status.blackStatus().toString())
-                                                   .header("white", status.whiteStatus().toString())
-                                                   .header("turn", status.turn().toString())
-                                                   .body(body))
-                        .orElseGet(() -> ResponseEntity.notFound().build());
+        return game.at(to)
+                   .filter(v -> v.piece().isPresent())
+                   .map(transformer::toPieceInfo)
+                   .map(body -> ResponseEntity.ok()
+                                              .header("black", status.blackStatus().toString())
+                                              .header("white", status.whiteStatus().toString())
+                                              .header("turn", status.turn().toString())
+                                              .body(body))
+                   .orElseGet(() -> ResponseEntity.notFound().build());
     }
 
-    @GetMapping("/pieces")
-    ResponseEntity<List<PieceDTO>> allPieces() {
-        List<PieceDTO> result = chessGame.notEmptySquares()
-                                         .stream()
-                                         .map(transformer::toPieceInfo)
-                                         .sorted((o1, o2) -> ObjectTransformer.POINTINFO_COMPARATOR.compare(o1.pointInfo(), o2.pointInfo()))
-                                         .toList();
+    @GetMapping("/game/{id}/pieces")
+    ResponseEntity<List<PieceDTO>> allPieces(@PathVariable("id") UUID id) {
+        var game = games.get(id);
+        if (Objects.isNull(game)) {
+            return ResponseEntity.notFound().build();
+        }
+
+        List<PieceDTO> result = game.notEmptySquares()
+                                    .stream()
+                                    .map(transformer::toPieceInfo)
+                                    .sorted((o1, o2) -> POINTINFO_COMPARATOR.compare(o1.pointInfo(), o2.pointInfo()))
+                                    .toList();
+
         return ResponseEntity.ok(result);
     }
 
-    @GetMapping("/pieces/{chessPoint}")
-    ResponseEntity<PieceDetailDTO> pieceDetail(@PathVariable String chessPoint) {
+    @GetMapping("/game/{id}/pieces/{chessPoint}")
+    ResponseEntity<PieceDetailDTO> pieceDetail(@PathVariable("id") UUID id, @PathVariable String chessPoint) {
+        var game = games.get(id);
+        if (Objects.isNull(game)) {
+            return ResponseEntity.notFound().build();
+        }
+
         Point point = transformer.toPoint(chessPoint);
         if (Objects.isNull(point)) {
             return ResponseEntity.badRequest().build();
         }
 
-        return chessGame.at(point)
-                        .filter(v -> v.piece().isPresent())
-                        .map(square -> transformer.toPieceDetail(square, chessGame.allowedMoves(point)))
-                        .map(ResponseEntity::ok)
-                        .orElseGet(() -> ResponseEntity.notFound().build());
+        return game.at(point)
+                   .filter(v -> v.piece().isPresent())
+                   .map(square -> transformer.toPieceDetail(square, game.allowedMoves(point)))
+                   .map(ResponseEntity::ok)
+                   .orElseGet(() -> ResponseEntity.notFound().build());
     }
 
 }
